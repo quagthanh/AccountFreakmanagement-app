b0038e4b14f19c7e1043a64fd725872eeb636e00
quagthanh <dinhquangthanh11@gmail.com>
2025-01-06 08:55:18 +0700

    init project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { AntdRegistry } from '@ant-design/nextjs-registry';
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 
-import '@/app/globals.css';
+import "@/app/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,10 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AntdRegistry>
-          {children}
-        </AntdRegistry>
-
+        <AntdRegistry>{children}</AntdRegistry>
       </body>
     </html>
   );
