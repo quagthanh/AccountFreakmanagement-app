55762aaff41680b587a2a5ffe4793164cb306e02
quagthanh <dinhquangthanh11@gmail.com>
2025-01-19 15:46:24 +0700

    pc 8:20:40

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 import { signIn } from "@/auth";
 import { errorMonitor } from "events";
+import { InvalidEmailPasswordError } from "./error";
 
 export async function authenticate(email: string, password: string) {
   try {
@@ -12,7 +13,19 @@ export async function authenticate(email: string, password: string) {
     });
     return r;
   } catch (error) {
-    return { error: "Incorrect username or password" };
+    if ((error as any).name === "InvalidEmailPasswordError") {
+      return {
+        error: (error as any).type,
+        code: 1,
+      };
+    } else if ((error as any).name === "InactiveAccountError") {
+      return {
+        error: (error as any).type,
+        code: 2,
+      };
+    } else {
+      return { error: "Internal server error", code: 0 };
+    }
 
     // if (error.cause.err instanceof InvalidLoginError) {
     //     return {"error": "Incorrect username or password"}
